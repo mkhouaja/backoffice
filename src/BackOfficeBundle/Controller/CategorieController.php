bbb2dbc5a8daa5be8ba2041fb01d3a8652cb5fae
mkhouaja <manel.khouaja@gmail.com>
2017-11-28 00:05:36 +0100

    CRUD catégorie

---
+++ src/BackOfficeBundle/Controller/CategorieController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace BackOfficeBundle\Controller;
+
+use BackOfficeBundle\Entity\Categorie;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Categorie controller.
+ *
+ */
+class CategorieController extends Controller
+{
+    /**
+     * @Route("/categories", name="categories")
+    */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $categories = $em->getRepository('BackOfficeBundle:Categorie')->findAll();
+
+        return $this->render('BackOfficeBundle:Pages:categories.html.twig', array(
+            'categories' => $categories,
+        ));
+    }
+    /**
+    * @Route("/categorie/ajout", name="ajout_categorie")
+    */
+    public function newAction(Request $request)
+    {
+        $categorie = new Categorie();
+        $form = $this->createForm('BackOfficeBundle\Form\CategorieType', $categorie, array(
+            'action' => $this->generateUrl('ajout_categorie'),
+            'method' => 'POST'
+        ));
+        $form->handleRequest($request);
+        //var_dump($request);
+        if ($form->isSubmitted()) {
+            $em = $this->getDoctrine()->getManager();
+            $date = date('Y-m-d');
+            $date = new \DateTime($date);
+            $categorie->setDateAjout($date);
+            $categorie->setDateModification($date);
+           
+            $em->persist($categorie);
+            $em->flush();
+
+            return $this->redirectToRoute('categories');
+        }
+
+        return $this->render('BackOfficeBundle:Pages:ajouter-categorie.html.twig', array(
+            'categorie' => $categorie,
+            'form' => $form->createView(),
+        ));
+    }
+
+
+    /**
+    * @Route("/categorie/modifier/{categorie}", name="modifier_categorie")
+    */
+    public function editAction(Request $request, Categorie $categorie)
+    {
+        // $deleteForm = $this->createDeleteForm($categorie);
+        $editForm = $this->createForm('BackOfficeBundle\Form\CategorieType', $categorie);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+             $date = date('Y-m-d');
+            $date = new \DateTime($date); 
+            $categorie->setDateModification($date);
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('categories');
+        }
+
+        return $this->render('BackOfficeBundle:Pages:modifier-categorie.html.twig', array(
+            'categorie' => $categorie,
+            'edit_form' => $editForm->createView()
+        ));
+    }
+
+    /**
+    * @Route("/categorie/supprimer/{categorie}", name="supprimer_categorie")
+    */
+    public function deleteAction(Request $request, Categorie $categorie)
+    {
+            if ($categorie) {
+                $em = $this->getDoctrine()->getManager();
+                $em->remove($categorie);
+                $em->flush();
+            }
+
+        return $this->redirectToRoute('categories');
+    }
+}
